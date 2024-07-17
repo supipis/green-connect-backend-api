aacca8182ac6593ced3c100643616e26943d2f1e
supipis <nirmanialgama@gmail.com>
2024-07-17 14:22:02 +0200

    set up the project

---
+++ src/test/java/com/example/green_connect_backend_api/GreenConnectBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.green_connect_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenConnectBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
