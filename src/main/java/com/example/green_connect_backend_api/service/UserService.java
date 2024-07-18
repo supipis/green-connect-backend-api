2e736271da068b8f397e8207628287ee5ad3ee65
supipis <nirmanialgama@gmail.com>
2024-07-18 09:28:43 +0200

    define user service

---
+++ src/main/java/com/example/green_connect_backend_api/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.green_connect_backend_api.service;
+
+import com.example.green_connect_backend_api.entity.User;
+import com.example.green_connect_backend_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByUsername(username);
+    }
+}
