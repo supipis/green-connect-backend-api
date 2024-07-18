38daa17afaf552b2c65abccd5b025a0fad33f0e6
supipis <nirmanialgama@gmail.com>
2024-07-18 19:17:40 +0200

    setup tips entity

---
+++ src/main/java/com/example/green_connect_backend_api/entity/Post.java
@@ -0,0 +1,57 @@
+package com.example.green_connect_backend_api.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author; // User who posted the message
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date postedOn = new Date(); // Automatically set to current date/time
+
+    public Post() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getPostedOn() {
+        return postedOn;
+    }
+
+    public void setPostedOn(Date postedOn) {
+        this.postedOn = postedOn;
+    }
+}
